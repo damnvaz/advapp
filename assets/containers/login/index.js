60d154981bfff3ec04e4f6c47c4df435b48c35bf
damnvaz <contact.daniel.vaz@gmail.com>
2023-03-12 16:09:41 -0300

    login page done

--- assets/containers/login/index.js
+++ assets/containers/login/index.js
@@ -1,4 +1,14 @@
-import { Title, Subtitle, ArrowBack, Input, InputLabel, ButtonLink, Button, Toast, Loading } from "../../components/index.js";
+import {
+  Title,
+  Subtitle,
+  ArrowBack,
+  Input,
+  InputLabel,
+  ButtonLink,
+  Button,
+  Toast,
+  Loading,
+} from "../../components/index.js";
 import { performLogin } from "../../queries/base.js";
 import { checkUserLanguage } from "../../utils/checkUserLanguage.js";
 import { loginText } from "./loginText.js";
@@ -15,11 +25,10 @@ function getPageContent() {
     <span id="copyrightText" class="copyrightText copyright-data">© Copyright ${year}</span><br /><br />
   `;
 
-
   let form = `
     <section id="loginForm" class="section form">
           <div class="container form-area">
-            <div class="form-group" style="display: table; margin: 20px auto 5px auto;">
+            <div class="form-group" style="display: table; margin: 35px auto 5px auto;">
                 ${InputLabel(loginText(lang)?.emailOrPhone)}
                 ${Input("emailLogin", "", "text", "", null)}
             </div>
@@ -34,7 +43,13 @@ function getPageContent() {
             </div>
             
             <div class="form-group" style="display: table; margin: 20px auto 40px auto;">
-                ${Button(loginText(lang)?.loginButton, login())}
+                <button
+                    class="button"
+                    type="button"
+                    id="loginbutton"
+                >
+                    ${loginText(lang)?.loginButton}
+                </button>
             </div>
 
             <div class="form-group row" style="display: table; margin: 5px auto 10px auto;">
@@ -48,7 +63,7 @@ function getPageContent() {
         <section class="section">
             <div class="container form-container">
                 ${ArrowBack()}
-                ${Title("iusok - Login", 'white')}
+                ${Title("Login", "white")}
             </div>
         </section>
       ${form}
@@ -66,6 +81,10 @@ function getPageContent() {
     document.getElementById("emailLogin").value = em;
   }
 
+  document.querySelector("#loginbutton").addEventListener("click", () => {
+    login();
+  });
+
   Loading(false);
 }
 
@@ -80,6 +99,10 @@ async function login() {
   if (!validateFields()) {
     Loading(false);
     return;
+  }
+
+  if (!login.includes('@')) {
+    login.replace(/\D/g, "");
   }
 
   const aux = await performLogin(login.trim(), pass.trim());
@@ -104,7 +127,7 @@ function validateFields() {
   const pass = document.querySelector("#senhaLogin").value;
 
   if (email === "") {
-    Toast("danger", "Por favor, preencha o email.");
+    Toast("danger", "Por favor, preencha o email ou telefone celular.");
     return false;
   }
 
