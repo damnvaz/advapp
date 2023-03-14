799f5e663128798dd71ea20c528a8d7cf8a4a5b6
damnvaz <contact.daniel.vaz@gmail.com>
2023-03-14 02:13:08 -0300

    add rg mask

--- assets/utils/maskInput.js
+++ assets/utils/maskInput.js
@@ -110,3 +110,12 @@ export function mCPFCNPJ(v) {
 
   return v;
 }
+
+export function mRG(v) {
+  v = v.replace(/\D/g, "");
+  v = v.replace(/(\d{2})(\d)/, "$1.$2");
+  v = v.replace(/(\d{3})(\d)/, "$1.$2");
+  v = v.replace(/(\d{3})(\d)/, "$1-$2");
+
+  return v;
+}
