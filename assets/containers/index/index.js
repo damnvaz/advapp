d2472fc122830a09a01fc46b6dad9d0c54214fb2
damnvaz <contact.daniel.vaz@gmail.com>
2023-03-17 15:03:16 -0300

    index improvements

--- assets/containers/index/index.js
+++ assets/containers/index/index.js
@@ -5,6 +5,7 @@ import {
   Subtitle,
   Button,
 } from "../../components/index.js";
+import { config } from "../../queries/base.js";
 import { checkUserLanguage } from "../../utils/checkUserLanguage.js";
 import { indexText } from "./indexText.js";
 
@@ -12,125 +13,102 @@ function indexHero(lang) {
   const event = `window.location.href='signup.html'`;
 
   return `
-    <div class="hero-container">
+    <div class="index-hero-container">
       <div>
-      <p class="hero-title"> iusok <p>
-        <p class="hero-description">${indexText(lang)?.heroDescription}</p>
+        <p class="index-hero-title">${config().platformName}<p>
+        <p class="index-hero-description">${indexText(lang)?.heroDescription}</p>
         ${Button(indexText(lang)?.joinNow, event)}
       </div>
-      <img src="assets/icons/balance.svg" class="balance" alt="balance" />
+      <img src="assets/icons/balance.svg" 
+        class="index-hero-img" 
+        alt="balance" />
     </div>
   `;
 }
 
-function indexTools() {
-  const lang = checkUserLanguage();
-
+function indexTools(lang) {
   return `
-  <div class="container-legal-tool">
-    <img src="assets/icons/hands-shaking.svg" class="hands-shake" alt="hands-shaking" />
-    <div class="yourLegalAndEffectiveTool area">
-      <p  class="title">${indexText(lang)?.yourLegalAndEffectiveToolTitle}</p>
+  <div class="legal-tool-container">
+    <img src="assets/icons/hands-shaking.svg" 
+      class="legal-tool-img" 
+      alt="hands-shaking" />
+    <div class="legal-tool-content">
+      <p class="legal-tool-title">${indexText(lang)?.yourLegalAndEffectiveToolTitle}</p>
       ${Subtitle(indexText(lang)?.yourLegalAndEffectiveToolDescription)}
     </div>
   </div>
   `;
 }
 
-function indexServicesOffered() {
-  const lang = checkUserLanguage();
-
+function indexServicesOffered(lang) {
   const list = [
     {
-      item1: indexText(lang)?.schedule,
-      item2: indexText(lang)?.proceduralManagement,
-      item3: indexText(lang)?.proceduralNotifications,
-      item4: indexText(lang)?.financialManagement,
-      item5: indexText(lang)?.petitionsModel,
-      item6: indexText(lang)?.contractModel,
+      item: indexText(lang)?.schedule,
     },
+    { item: indexText(lang)?.proceduralManagement },
+    { item: indexText(lang)?.proceduralNotifications },
+    { item: indexText(lang)?.financialManagement },
+    { item: indexText(lang)?.petitionsModel },
+    { item: indexText(lang)?.contractModel },
     {
-      item1: indexText(lang)?.helpForum,
-      item2: indexText(lang)?.custumerManagement,
-      item3: indexText(lang)?.filesManagement,
-      item4: indexText(lang)?.tableValueOAB,
-      item5: indexText(lang)?.fitsInYourPocket,
-      item6: indexText(lang)?.andMuchMore,
+      item: indexText(lang)?.helpForum,
     },
+    { item: indexText(lang)?.custumerManagement },
+    { item: indexText(lang)?.filesManagement },
+    { item: indexText(lang)?.tableValueOAB },
+    {
+      item: indexText(lang)?.fitsInYourPocket,
+    },
+    { item: indexText(lang)?.andMuchMore },
   ];
 
   let el = "";
   for (let i = 0; i < list.length; i++) {
-    el += `<div>
-        <div class="advantage-container">
-          <img src="assets/icons/check.svg" class="check-icon" alt="check icon" /> 
-          <span class="advantage-item">${list[i].item1}</span>
-        </div>
-        <div class="advantage-container">
-          <img src="assets/icons/check.svg" class="check-icon" alt="check icon" /> 
-          <span class="advantage-item">${list[i].item2}</span>
-        </div>
-        <div class="advantage-container">
-          <img src="assets/icons/check.svg" class="check-icon" alt="check icon" /> 
-          <span class="advantage-item">${list[i].item3}</span>
-        </div>
-        <div class="advantage-container">
-          <img src="assets/icons/check.svg" class="check-icon" alt="check icon" /> 
-          <span class="advantage-item">${list[i].item4}</span>
-        </div>
-        <div class="advantage-container">
-          <img src="assets/icons/check.svg" class="check-icon" alt="check icon" /> 
-          <span class="advantage-item">${list[i].item5}</span>
-        </div>
-        <div class="advantage-container">
-          <img src="assets/icons/check.svg" class="check-icon" alt="check icon" /> 
-          <span class="advantage-item">${list[i].item6}</span>
-        </div>
+    if (i === 0) {
+      el += `<div>`;
+    }
+
+    el += `<div class="services-container">
+          <img src="assets/icons/check.svg" 
+            class="services-check-icon" 
+            alt="check icon" /> 
+          <span class="services-item">${list[i].item}</span>
       </div>`;
+
+    if (i === 5) {
+      el += `</div><div>`;
+    }
+
+    if (i === list.length - 1) {
+      el += `</div>`;
+    }
   }
 
   return `
-    <div class="advantages d-table mx-auto area">
+    <div class="services">
       ${Title(indexText(lang)?.whatWeOffer, "white")}
       <br>
-        <div class="columns">
-          ${el}
-        </div>
-      </div>     
-  `;
-}
-
-function indexSignupNow() {
-  const lang = checkUserLanguage();
-
-  const event = `window.location.href='signup.html'`;
-
-  return `
-    <div class="mt-5 d-flex flex-column area">
-      ${Title(indexText(lang)?.whatAreYouWaitingForTitle)}
-      ${Subtitle(indexText(lang)?.whatAreYouWaitingForDescription)}
-      <div class="d-flex align-items-center flex-column mb-5">
-        ${Button(indexText(lang)?.signUpNow, event)}
+      <div class="services-columns">
+        ${el}
       </div>
-    </div>
+    </div>     
   `;
 }
 
-function content() {
-  const lang = checkUserLanguage();
-
-  document.querySelector("#content").innerHTML = `
-    <section class="section container mt-5">
-      ${indexHero(lang)}
-      ${indexTools()}
-      ${indexServicesOffered()}
-      ${indexSignupNow()}
-    </section>`;
-}
 
 function PageContent() {
+  const lang = checkUserLanguage();
+
   Navbar();
-  content();
+  document.querySelector("#content").innerHTML = `
+    <section class="section-area section-area-index">
+      <div class="content">
+        ${indexHero(lang)}
+        ${indexServicesOffered(lang)}
+        ${indexTools(lang)}
+      </div>
+    </section>
+  `;
   Footer();
 }
 
--- assets/containers/index/indexText.js
+++ assets/containers/index/indexText.js
@@ -1,60 +1,55 @@
-  
 export function indexText(language) {
+  const languages = [
+    {
+      heroDescription: "Foque no que importa: <strong>sua advocacia</strong>.",
+      yourLegalAndEffectiveToolTitle:
+        "A <span>sua</span> ferramenta jurídica eficaz",
+      yourLegalAndEffectiveToolDescription:
+        "Aqui, advogar ficou mais fácil. A marcação de consultas é simples, os relatórios saem na hora e você ainda pode tirar dúvidas com outros usuários.",
+      whatWeOffer: "O que oferecemos:",
+      schedule: "Agenda online",
+      proceduralManagement: "Gestão processual",
+      proceduralNotifications: "Notificações processuais",
+      financialManagement: "Gestão financeira",
+      petitionsModel: "Modelos de petições",
+      contractModel: "Contratos Modelos",
+      helpForum: "Fórum de ajuda",
+      custumerManagement: "Gerenciamento de clientes",
+      filesManagement: "Gerenciamento de arquivos",
+      tableValueOAB: "Tabela de valores OAB",
+      fitsInYourPocket: "Cabe no seu bolso",
+      andMuchMore: "E muito mais",
+      signUpNow: "Cadastre-se agora",
+      joinNow: "Faça parte agora",
+    },
+    {
+      heroDescription: "Focus on what matters: <strong>your advocacy</strong>.",
+      yourLegalAndEffectiveToolTitle: "<span>Your</span> effective legal tool",
+      yourLegalAndEffectiveToolDescription:
+        "Here, advocating became easier. Scheduling appointments is simple, reports are available in real time and you can even share doubts with others users.",
+      whatWeOffer: "What we offer:",
+      schedule: "Online schedule",
+      proceduralManagement: "Procedural management",
+      proceduralNotifications: "Procedural notifications",
+      financialManagement: "Financial management",
+      petitionsModel: "Model petitions",
+      contractModel: "Model contract",
+      helpForum: "Help morum",
+      custumerManagement: "Custumer management",
+      filesManagement: "Files management:",
+      tableValueOAB: "Table value OAB:",
+      fitsInYourPocket: "Fit is in your pocket",
+      andMuchMore: "And much more",
+      signUpNow: "Sign up now",
+      joinNow: "Join Now",
+    },
+  ];
 
-    const languages = [
-      {
-        heroDescription: 'Foque no que importa: <strong>sua advocacia</strong>.',
-        yourLegalAndEffectiveToolTitle: 'A <span>Sua</span> ferramenta jurídica eficaz',
-        yourLegalAndEffectiveToolDescription: 'Com a nossa plataforma, advogar ficou mais fácil. A marcação de consultas é simples, os relatórios saem na hora e você ainda pode tirar dúvidas com outros usuários.',
-        whatWeOffer: 'O que oferecemos:',
-        schedule: 'Agenda online',
-        proceduralManagement: 'Gestão processual',
-        proceduralNotifications: 'Notificações processuais',
-        financialManagement: 'Gestão financeira',
-        petitionsModel: 'Modelos de petições',
-        contractModel: 'Contratos Modelos',
-        helpForum: 'Fórum de ajuda',
-        custumerManagement: 'Gerenciamento de clientes',
-        filesManagement: "Gerenciamento de arquivos",
-        tableValueOAB: 'Tabela de valores OAB',
-        fitsInYourPocket: 'Cabe no seu bolso',
-        andMuchMore: 'E muito mais',
-        whatAreYouWaitingForTitle: 'Tá esperando o que?',
-        whatAreYouWaitingForDescription: 'Tenha uma gestão completa sobre o seu ativo mais valioso: seus clientes. Abra sua agenda online agora mesmo!',
-        signUpNow: 'Cadastre-se agora',
-        joinNow: 'Faça parte agora',
-      },
-      {
-        heroDescription: 'Focus on what matters: <strong>your advocacy</strong>.',
-        yourLegalAndEffectiveToolTitle: '<span>Your</span> effective legal tool',
-        yourLegalAndEffectiveToolDescription: 'On our platform, advocating became easier. Scheduling appointments is simple, reports are available in real time and you can even share doubts with others users.',
-        whatWeOffer: 'What we offer:',
-        schedule: 'Online schedule',
-        proceduralManagement: 'Procedural management',
-        proceduralNotifications: 'Procedural notifications',
-        financialManagement: 'Financial management',
-        petitionsModel: 'Model petitions',
-        contractModel: 'Model contract',
-        helpForum: 'Help morum',
-        custumerManagement: 'Custumer management',
-        filesManagement: 'Files management:',
-        tableValueOAB: 'Table value OAB:',
-        fitsInYourPocket: 'Fit is in your pocket',
-        andMuchMore: 'And much more',
-        whatAreYouWaitingForTitle: 'What are you waiting for?',
-        whatAreYouWaitingForDescription: 'Get a complete report about your more valuable active: your clients. Open your online schedule right now',
-        signUpNow: 'Sign up now',
-        joinNow: 'Join Now',
-      },
-      
-    ];
-  
-    switch (language) {
-      case "pt-BR":
-        return languages[0];
-  
-      default:
-        return languages[1];
-    }
-  };
-    
+  switch (language) {
+    case "pt-BR":
+      return languages[0];
+
+    default:
+      return languages[1];
+  }
+}
